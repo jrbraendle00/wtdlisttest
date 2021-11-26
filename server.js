8594d983cc0dd9e36a27e186f51b4c2df4edfff8
jrbraendle00 <mydeskisbroken@gmail.com>
2021-11-26 13:55:17 -0500

    fix 12

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 
-var path = require('path');
+//var path = require('path');
 
 const port = process.env.PORT || 3000;
 
-process.env.PWD = process.cwd();
+//process.env.PWD = process.cwd();
 
-app.use(express.static(path.join(process.env.PWD, 'public')));
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
     
