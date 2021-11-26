f0afba3e98fdf1bd1a6ac1e13732840254df02f8
jrbraendle00 <mydeskisbroken@gmail.com>
2021-11-26 13:32:01 -0500

    fix 10

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function(req, res) {
     
