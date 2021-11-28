3a2e1fce272df8acf6c373db8db1b541ce087188
jrbraendle00 <mydeskisbroken@gmail.com>
2021-11-28 14:11:24 -0500

    testing db conn

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/* const express = require('express');
 const app = express();
 
 const path = require('path');
@@ -12,4 +12,40 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname+'/public/frontend.html'));
 });
 
-app.listen(port);
+app.listen(port); */
+
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+
+const sql = require('mysql');
+
+
+
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    var config = sql.createPool({
+        host: 'us-cdbr-east-04.cleardb.com',
+        user: 'baeffcfa0361e8',
+        password: 'd75d807f',
+        database: 'heroku_f4a58d3b92ef321'
+    });
+    
+    module.exports = config;
+    
+    var sql = "select * from task_list";
+    config.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log("record retrieved");
+        res.send(result);
+    });
+    
+    //res.sendFile(path.join(__dirname+'/public/frontend.html'));
+});
+
+app.listen(port); 
