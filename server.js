98772520013996b09b90dd94f099af0eabca371d
jrbraendle00 <mydeskisbroken@gmail.com>
2021-11-28 19:13:21 -0500

    testing db conn

--- server.js
+++ server.js
@@ -25,23 +25,6 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-    var sql = require('mysql');
-
-    var config = sql.createPool({
-        host: 'us-cdbr-east-04.cleardb.com',
-        user: 'baeffcfa0361e8',
-        password: 'd75d807f',
-        database: 'heroku_f4a58d3b92ef321'
-    });
-    
-    module.exports = config;
-    
-    var sql = "select * from task_list";
-    config.query(sql, function(err, result) {
-        if (err) throw err;
-        console.log("record retrieved");
-        res.send(result);
-    });
     
     res.sendFile(path.join(__dirname+'/public/frontend.html'));
 });
